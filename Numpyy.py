485d2345dacbb9ce0e447687ca6c0d8a31dd35ee
theram600-arch <theram600@gmail.com>
2026-04-29 23:20:40 +0530

    understand dimensions in numpy

---
+++ Numpyy.py
@@ -0,0 +1,25 @@
+import numpy as np
+import random
+
+
+
+# arr=np.array([1,2,3,4,5])
+# print(type(arr))
+
+# print(np.__version__)
+
+
+
+# two_dimensional_list = np.array([[0,1,2], [3,4,5], [6,7,8]])
+# print(type(two_dimensional_list))
+
+
+
+                                                                                        #  tolist:  convert array onto list
+# two_dimensional_list = np.array([[0,1,2], [3,4,5], [6,7,8]],dtype=float)
+# print(two_dimensional_list)
+
+# convert=two_dimensional_list.tolist()
+# print(type(convert))
+# print(convert)
+
