b90472f0daf8484575dd2c0c36f9cdbd9101c506
theram600-arch <theram600@gmail.com>
2026-04-29 23:22:03 +0530

    learn size and adding in numpy

--- Numpyy.py
+++ Numpyy.py
@@ -23,3 +23,37 @@ import random
 # print(type(convert))
 # print(convert)
 
+
+
+
+
+                                                                                        #   shape: show how many rows and column in it , first show rows and second columns                                         
+# two_dimensional_list = np.array([[0,1,2], [3,4,5], [6,7,8]])
+# print(two_dimensional_list.shape)
+# tw = np.array([[0,1,2,3,5], 
+#                 [3,4,5,6,8],                                    
+#                 [6,7,8,8,9]])
+# print(tw.shape)
+
+
+
+
+
+                                                                                #  size: show how many elements in array
+# tw = np.array([[0,1,2,3,5], 
+#                 [3,4,5,6,8],                                    
+#                 [6,7,8,8,9]])
+# print(tw.size)
+
+
+
+
+
+
+                                                                        #  we can perform operation like addition, subtraction, mutiplacation, division on it
+# tw = np.array([[0,1,2,3,5], 
+#                 [3,4,5,6,8],                                    
+#                 [6,7,8,8,9]])
+# oo=tw+10
+# print(oo)
+
